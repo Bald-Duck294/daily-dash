e549e8b2ca1a3321b8b3451256d81d1334c02a03
Goolu Moolu <goolumoolu294@gmail.com>
2026-09-23 19:04:12 +0530

    feat: add configured axios instance with request and response interceptors

--- shared/api/axios.instance.js
+++ shared/api/axios.instance.js
@@ -9,7 +9,7 @@ const storeState = store.getState();
 
 const axiosInstance = axios.create({
   // baseURL: "https://saaf-ai-backend.vercel.app/api",
-  // baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8001/api",
   // baseURL: "https://dash-backend-five.vercel.app/api",
   baseURL: "https://daily-dash-backend-development.vercel.app/api",
   headers: {
